219597997548af3c5c1de96d0a7889546dcdf0ba
ArdannyR <ardannyromero@gmail.com>
2026-05-13 15:13:26 -0500

    actividad

--- Clase 7/docker_file_1/servidor.py
+++ Clase 7/docker_file_1/servidor.py
@@ -1,1 +1,10 @@
 from flask import Flask
+
+servidor = Flask(__name__)
+
+@servidor.route("/")
+def hola():
+    return "Hola desde el Servidor"
+
+if __name__ == "__main__":
+    servidor.run(debug=True)
