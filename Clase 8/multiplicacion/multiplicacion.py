f6c5478e7bbbe9dfcb757bd993753c10f81e9f7b
ArdannyR <ardannyromero@gmail.com>
2026-05-20 15:11:38 -0500

    docker compose

---
+++ Clase 8/multiplicacion/multiplicacion.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+app.route('/multiplicacion', methods=['POST'])
+
+def multiplicacion():
+    data = request.json
+    a = data['a']
+    b = data['b']
+
+    resultado = a * b
+
+    return jsonify({
+        "operacion": "multiplicacion",
+        "resultado": resultado
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
