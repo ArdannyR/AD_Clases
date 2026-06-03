45e09f015e2b85464353e526e29d6ea21c829752
ArdannyR <ardannyromero@gmail.com>
2026-06-03 16:58:12 -0500

    clases

--- Practica_prueba_dos/XML-RCP/basico/cliente.py
+++
@@ -1,9 +0,0 @@
-import xmlrpc.client
-
-# Crea el proxy que apunta a la dirección del servidor
-proxy = xmlrpc.client.ServerProxy("http://localhost:5000")
-
-# Llama a la función remota "sumar"
-resultado = proxy.sumar(10, 5)
-
-print(f"El resultado de la suma remota es: {resultado}")
